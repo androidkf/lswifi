0e4003ea5a8fc43769232f92061e15c3790f59af
humingzheng <humingzheng@u51.com>
2018-08-15 16:59:15 +0800

    lswifi

---
+++ lswifi.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+#author:hmz
+#Disclaimer iwconfig wlan0 scan for linux
+# lsusb (list wifi SSIDs) utility for Mac OS X
+# Author: HMZ, AUG 2018
+/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -s
